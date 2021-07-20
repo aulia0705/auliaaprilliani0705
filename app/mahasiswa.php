0e47ed425d6aaca44450c578fce28fe696a69f2f
aulia0705 <aprilianiaulia15@gmail.com>
2021-07-20 19:29:28 +0800

    tugas uas web

---
+++ app/mahasiswa.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class mahasiswa extends Model
+{
+    protected $table      = 'mahasiswa';
+    protected $fillable   =['id','user_id','npm','tempat_lahir','tgl_lahir','gender','telepon','alamat'];
+    public $timestamps    =false;
+
+    public function User()
+{
+    return $this->belongsTo(User::class, 'user_id','id');
+}
+
+    
+}
